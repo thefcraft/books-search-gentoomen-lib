1fc270aba42e6f0cd2e79b4386c98ca13b313a88
Thefcraft <76678136+thefcraft@users.noreply.github.com>
2025-01-11 17:26:48 +0530

    suspense

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -14,6 +14,7 @@ import { useSearchParams } from 'next/navigation'
 import Link from 'next/link'
 
 export default function BookPage() {
+    
     const searchParams = useSearchParams()
     const index = searchParams.get('index')
     if (!index) {
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from 'react';
 import "./globals.css";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
       </body>
     </html>
   );
